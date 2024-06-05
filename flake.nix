38c90d662bdd4359c17e92d9a7d7e8cceff9d0bd
Vanilla <osu_Vanilla@126.com>
2024-06-05 21:34:55 +0800

    feat[flake.nix]: We got builtable raw img.

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    Jovian-NixOS.url = "github:Jovian-Experiments/Jovian-NixOS";
+    nixos-generators.url = "github:nix-community/nixos-generators";
+  };
+
+  outputs = { self, nixpkgs, Jovian-NixOS, nixos-generators }: rec {
+    nixosConfigurations."NixOS-SD" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        Jovian-NixOS.nixosModules.default
+        nixos-generators.nixosModules.raw
+      ] ++ [ ./configuration.nix ];
+    };
+
+    packages.x86_64-linux.default =
+      nixosConfigurations."NixOS-SD".config.system.build.raw;
+  };
+}
