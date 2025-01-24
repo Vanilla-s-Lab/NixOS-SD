5c8187b067d7e21dc04fa4947cc0274487877d3f
Vanilla <osu_Vanilla@126.com>
2025-01-24 18:20:29 +0800

    chore[home-manager/home.nix]: Add for Non-Steam games.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./fonts/conf.nix
     ./fonts/fontconfig.nix
+  ];
+
+  home.packages = [
+    pkgs.nautilus
+    pkgs.unzip
   ];
 
   home.stateVersion = "24.11";
