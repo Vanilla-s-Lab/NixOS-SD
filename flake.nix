1096a72c55280a6cb57a92105e0c48f72f328c66
Vanilla <osu_Vanilla@126.com>
2025-07-17 12:19:32 +0800

    chore[flake.nix]: Reorder.

--- flake.nix
+++ flake.nix
@@ -1,46 +1,46 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    Jovian-NixOS.url = "github:Jovian-Experiments/Jovian-NixOS";
-    nixos-generators.url = "github:nix-community/nixos-generators";
     deploy-rs.url = "github:serokell/deploy-rs";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    Jovian-NixOS.url = "github:Jovian-Experiments/Jovian-NixOS";
+    nixos-generators.url = "github:nix-community/nixos-generators";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: rec {
+  outputs = { home-manager, nixpkgs, ... }@inputs: rec {
+    deploy.nodes."NixOS-SD" = {
+
+      fastConnection = true;
+      hostname = "NixOS-SD.lan";
+      magicRollback = false;
+
+      profiles.system = {
+        path = with inputs;
+          deploy-rs.lib.x86_64-linux.activate.nixos
+            self.nixosConfigurations."NixOS-SD";
+
+        sshUser = "root";
+      };
+    };
+
     nixosConfigurations."NixOS-SD" = nixpkgs.lib.nixosSystem {
-
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-
       modules = with inputs; [
+        ./configuration.nix
+        ./home-manager
+
+        home-manager.nixosModules.home-manager
         Jovian-NixOS.nixosModules.default
         nixos-generators.nixosModules.raw-efi
-
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        ./home-manager
       ];
+
+      specialArgs = { inherit inputs; };
+      system = "x86_64-linux";
     };
 
     packages.x86_64-linux.default =
       nixosConfigurations."NixOS-SD".config.system.build.raw;
-
-    deploy.nodes."NixOS-SD" = {
-      hostname = "NixOS-SD.lan";
-
-      fastConnection = true;
-      magicRollback = false;
-
-      profiles.system = {
-        sshUser = "root";
-
-        path = with inputs;
-          deploy-rs.lib.x86_64-linux.activate.nixos
-            self.nixosConfigurations."NixOS-SD";
-      };
-    };
   };
 }
